a478609365f64f5270a78d8c6e6dad0144fc5bdf
Amra Mehmedovic <bajric.armin.3t1@gmail.com>
2018-11-04 23:34:11 +0100

    implementirana konekcije na myslq bazu te provjera logina korisnika

--- server.js
+++ server.js
@@ -5,25 +5,55 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 var path = require('path');
  var cookie = require('cookie');
-	
+	app.use(express.static('pages'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pages')));
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 
 });
 
-app.post('/login', (req, res) => {
-  const email = {email:JSON.stringify(req.body.email)};
-	const password= {email:JSON.stringify(req.body.password)};
+app.post('/userForm', (req, res) => {
+ // const email = {email:JSON.stringify(req.body.email)};
+//	const password= {password:JSON.stringify(req.body.password)};
+	const email=req.body.email;
+	const password=req.body.password;
  var mysql = require('mysql');
-
+console.log(email);
+	console.log(password);
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "yourpassword",
+  password: "",
   database: "webimenik"
 });
-	 
+	var post  = {email: email, password :password};
+var fields;
+	 con.connect(function(err) {
+  if (err) {throw err;}
+  con.query("SELECT * FROM  korisnik WHERE email='"+email+"' AND password='"+password+"'", function (err, result, fields) {
+    if (err)
+		{throw err;}
+	  else
+		  {
+			  if(result.length>0)
+				 {
+					
+				{		 
+					console.log("radiiiiiiiii");
+				 res.redirect('/user');
+					
+				}
+				} 
+		  }
+	  
+    console.log(result);
+  });
+});
+	app.get('/user', function(req, res) {
+		res.type('html');
+    res.send(`${__dirname}/userForm.html`);
+});
 	res.setHeader('Set-Cookie', cookie.serialize('Email', String(req.body.email), {
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 7 // 1 week
